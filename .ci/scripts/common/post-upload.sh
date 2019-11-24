a5d008005c730c54f3fa8e22ce9811cda5a6a331
dampih <dampih@gmail.com>
2019-11-24 13:00:31 +0700

    dssdffdsfdsdsfsdf

--- .ci/scripts/common/post-upload.sh
+++ .ci/scripts/common/post-upload.sh
@@ -9,10 +9,10 @@ cp "${REV_NAME}-source.tar.xz" "$DIR_NAME"
 
 tar $COMPRESSION_FLAGS "$ARCHIVE_NAME" "$DIR_NAME"
 
-mv "$DIR_NAME" $RELEASE_NAME
-mv "${REV_NAME}-source.tar.xz" $RELEASE_NAME
+mv "$DIR_NAME" "TESSSSS"
+mv "${REV_NAME}-source.tar.xz" "TESSSSS"
 
-7z a "$REV_NAME.7z" $RELEASE_NAME
+7z a "$REV_NAME.7z" "TESSSSS"
 
 # move the compiled archive into the artifacts directory to be uploaded by travis releases
 mv "$ARCHIVE_NAME" artifacts/
